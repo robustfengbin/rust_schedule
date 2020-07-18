1c4e2ae001ae58b930bac67f1afe52fdb226a368
fengbin <27317142@qq.com>
2020-07-18 18:15:41 +0800

    bind 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use rust_schedule::ThreadPool;
 fn main(){
     println!("hello,main");
     let pool = ThreadPool::new(4);
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
