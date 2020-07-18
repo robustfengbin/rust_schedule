03bd4c9c70acdcdc858703d91cfc7b56896909de
fengbin <27317142@qq.com>
2020-07-18 17:05:03 +0800

    modify main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,11 @@ use std::env;
 const INTERVAL_TIME:u64 = 10000;
 #[tokio::main]
 async fn main() {
+
+     loop_schedule().await;
+}
+
+async fn loop_schedule(){
     let args: Vec<String> = env::args().collect();
     let mut command_name = "dir";
     if args.len()>1{
@@ -17,10 +22,11 @@ async fn main() {
     let mut interval = time::interval(Duration::from_millis(INTERVAL_TIME));
     loop {
         interval.tick().await;
-        schedual_start_eth(command_name.to_string());
+        schedule_start(command_name.to_string());
     }
 }
-fn schedual_start_eth(command_str:String){
+
+fn schedule_start(command_str:String){
     block_on(exec(command_str));
 }
 async fn exec(command_str:String){
@@ -31,4 +37,6 @@ async fn exec(command_str:String){
     println!("the command '{}', run result :{:?}",command_str,result);
 
 }
+
+
 
