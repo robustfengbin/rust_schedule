92d166aec0e1abe88487557d0def1f3954104e39
fengbin <27317142@qq.com>
2020-09-16 14:31:29 +0800

    add size

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
 
-    for n in  0..2000{
+    let size = 1000000;
+    for n in  0..size{
         let s1 =  Student {name:  String::from("robust"),age:12};
         println!("n:{:?}",s1);
         println!("n:{}",n);
