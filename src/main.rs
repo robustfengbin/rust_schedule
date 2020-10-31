6c1953301d773ed632616c0b39ff1c541895b2cd
fengbin <27317142@qq.com>
2020-10-31 10:43:35 +0800

    add shell bash

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::process::Output;
 use tokio::time;
 use std::time::Duration;
 use futures::executor::block_on;
 use std::process::{Command};
 use clap::App;
+
 #[tokio::main]
 async fn main() {
      loop_schedule().await;
@@ -25,7 +27,7 @@ async fn loop_schedule(){
         println!("Value for TIME: {}", o);
         interval_time = o.parse().unwrap()
     }
-    println!("command_name:{}, time:{}",command_name,interval_time);
+    println!("command_name:{}, runing interval time:{} s",command_name,interval_time);
     let mut interval = time::interval(Duration::from_millis(interval_time));
     loop {
         interval.tick().await;
@@ -36,11 +38,12 @@ fn schedule_start(command_str:String){
     block_on(exec(command_str));
 }
 async fn exec(command_str:String){
-    let result  = 
+    let result:Output  = 
     Command::new(&command_str)
             .output()
             .expect("failed to execute process");
-    println!("the command '{}', run result :{:?}",command_str,result);
+            
+    println!("run result :{:?}",String::from_utf8(result.stdout));
 }
 
 
